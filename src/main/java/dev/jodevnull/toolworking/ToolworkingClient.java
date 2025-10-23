561bcdc05080f1a867e5f4dc7a54f584ddfd37ff
jo-devnull <jorge.devnull@gmail.com>
2025-10-23 17:59:16 -0300

    update: fix some recipes and refactor some code

---
+++ src/main/java/dev/jodevnull/toolworking/ToolworkingClient.java
@@ -0,0 +1,12 @@
+//package dev.jodevnull.toolworking;
+//
+//import dev.jodevnull.toolworking.ponder.TWPonderPlugin;
+//import net.createmod.ponder.foundation.PonderIndex;
+//
+//public class ToolworkingClient
+//{
+//    public ToolworkingClient()
+//    {
+//        PonderIndex.addPlugin(new TWPonderPlugin());
+//    }
+//}
